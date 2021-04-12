66ecf965a141603d26c01400f788cbb512a119d6
narogm <narogmateusz@gmail.com>
2021-04-13 00:46:40 +0200

    SVD

---
+++ svd/main.py
@@ -0,0 +1,50 @@
+import numpy as np
+from numpy.linalg import norm
+
+
+def power_method(A, eps):
+    vec = np.random.rand(A.shape[0])
+    vec = vec / norm(vec)
+    matrix = A @ A.T
+    while True:
+        prev_vec = vec
+        vec = matrix @ prev_vec
+        vec = vec / norm(vec)
+        if norm(vec - prev_vec) < eps:
+            return vec
+
+
+def custom_svd(A, eps=0.1):
+    values = []
+    for i in range(A.shape[0]):
+        matrix = A.copy()
+        for u, s, v in values[:i]:
+            tmp = np.outer(u, v)
+            matrix -= s * tmp
+
+        u = power_method(matrix, eps)
+        v_unormalized = A.T @ u
+        s = norm(v_unormalized)
+        v = v_unormalized / s
+        values.append((u, s, v))
+    u, s, v = [np.array(x) for x in zip(*values)]
+    return u.T, s, v
+
+
+if __name__ == '__main__':
+    A = np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]], dtype='float32')
+    # A = np.array([[1, 2, 0], [2, 0, 2]], dtype='float32')
+
+    u, s, v = custom_svd(A, 0.01)
+    np.set_printoptions(suppress=True, precision=3)
+
+    print(u @ np.diag(s) @ v)
+
+    print("\nLeft Singular Vectors:")
+    print(u)
+
+    print("\nSingular Values:")
+    print(np.diag(s))
+
+    print("\nRight Singular Vectors:")
+    print(v)
